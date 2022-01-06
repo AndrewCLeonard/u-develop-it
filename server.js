ce9c22db60421bb2cc30b7e9db68c71f39ba6b88
Andrew Leonard <andrewcleonard@fastmail.com>
2022-01-06 07:44:07 -0800

    adding POST route

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const res = require("express/lib/response");
 const mysql = require("mysql2");
-const app = express();
+const inputCheck = require("./utils/inputCheck");
 const PORT = process.env.PORT || 3001;
+const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -47,6 +48,7 @@ app.get("/api/candidates", (req, res) => {
 		});
 	});
 });
+
 // GET a single candidate
 app.get("/api/candidate/:id", (req, res) => {
 	const sql = `SELECT * FROM candidates WHERE id = ?`;
@@ -87,18 +89,28 @@ app.delete("/api/candidate/:id", (req, res) => {
 });
 
 // Create a candidate
-/*
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-                VALUES (?, ?, ?, ?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+app.post("/api/candidate", ({ body }, res) => {
+	const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+	if (errors) {
+		res.status(400).json({ error: errors });
+		return;
+	}
 
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
+	const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+	const params = [body.first_name, body.last_name, body.industry_connected];
+
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: "success",
+			data: body,
+		});
+	});
 });
-*/
 
 // default response for any other request (not found)
 app.use((req, res) => {
